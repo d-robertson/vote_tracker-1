4337ffdca4f44bdf2abc05a31a2cd57adef8a5c2
petertrung <petertrung@comcast.net>
2015-08-17 12:52:32 -0700

    vote

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var photoArray;
+
+ var Photo = function(fileLocation){//	constructor
+ 	this.path = fileLocation;
+ 	this.vote = 1;
+ 	this.index= //where it lives in photoArray
+ }
+
+ var Tracker = function(){
+ 	this.leftPhoto
+ 	this.rightPhoto
+
+ }
+
+Photo.prototype.highlight = function(first_argument) {
+	// highlight the photo after it is clicked
+};
+Tracker.prototype.receiveVote = function(first_argument) {
+	// receive the click
+	// increment the vote count
+	// event listener on each photo
+	// highlight()
+	// drawthechart()
+	// giveUseroptiontovoteAgain()
+};
+
+Tracker.prototype.getRandomInt = function(first_argument) {
+	// Display the randomly selected photo
+};
