16dc7578a36fe64c0db0d9d212f82374eeb9b92a
Derek Robertson <dwilliamrobertson@gmail.com>
2015-08-18 14:51:32 -0700

    work on click count

--- js/app.js
+++ js/app.js
@@ -12,23 +12,23 @@ $(document).ready(function(){
       value: 1,
       color: '#0000ff'
     }
-  ]
+  ];
 
   var catOptions = {
     segmentShowStroke : false,
     percentageInnerCutout: 75,
     animateScale : true
-  }
+  };
 //Chart
   var cats = document.getElementById('catChart').getContext('2d');
   var catChart = new Chart(cats).Pie(catData, catOptions);
  //Constructor Function
    var Photo = function(fileLocation){//  constructor
     this.path = fileLocation;
-    this.vote = 1;
+    this.vote = 0;
    }
 
-   //new objects 
+   //new objects
    var pic1 = new Photo('img/1.jpg');
    var pic2 = new Photo('img/2.jpg');
    var pic3 = new Photo('img/3.jpg');
@@ -55,67 +55,90 @@ $(document).ready(function(){
 //Connects all the functions
 var cat = function(){
   //hides reset button
-$('#reset').hide();
-//Generates Random Number to choose which number in the array
-  var randomPic = function() {
-    return Math.floor(Math.random()*(14 - 0));
-
-  };
-//Chooses Photo for left and right from the array
-   var Tracker = function(){
-    this.leftPhoto = photoArray[randomPic()];
-    this.rightPhoto = photoArray[randomPic()];
-    console.log(this.leftPhoto);
-    console.log(this.rightPhoto);
-//Makes it find a new photo if the right and the left images are the same
-    while (this.leftPhoto === this.rightPhoto) {
+  $('#reset').hide();
+  //Generates Random Number to choose which number in the array
+    var randomPic = function() {
+      return Math.floor(Math.random()*(14 - 0));
+    };
+  //Chooses Photo for left and right from the array
+    var Tracker = function(){
+      this.leftPhoto = photoArray[randomPic()];
       this.rightPhoto = photoArray[randomPic()];
+      console.log(this.leftPhoto);
+      console.log(this.rightPhoto);
+  //Makes it find a new photo if the right and the left images are the same
+      while (this.leftPhoto === this.rightPhoto) {
+        this.rightPhoto = photoArray[randomPic()];
+      }
+    };
+  //define the value of both sides of the graph
+     var tracker = new Tracker();
+     catData[0].value = tracker.leftPhoto.vote;
+     catData[1].value = tracker.rightPhoto.vote;
+  //Function registered if right pic is clicked
+    Tracker.prototype.rightVote = function(){
+      catChart.segments[0].value++
+      catChart.update();
+      };
+      //show reset
+      $('#reset').show();
+
+
+  //Function registered if left pic is clicked
+    Tracker.prototype.leftVote = function(){
+      catChart.segments[1].value++
+      catChart.update();
+      //show reset
+      $('#reset').show();
+
+    };
+
+      Tracker.prototype.addVote = function(selected) {
+        console.log(selected);
+        for(var i = 0; i < photoArray.length; i++) {
+          if(selected === photoArray[i].path) {
+            photoArray[i].vote++;
+            console.log(photoArray[i].vote);
+            return;
+
+          }
+        }
+      }
+  //Resets the score of the graph and picks a new image
+      Tracker.prototype.reset = function(){
+    catChart.segments[0].value = 1;
+    catChart.segments[1].value = 1;
+    catChart.update();
+    //hide reset
+    cat();
+    };
+
+
+    // Tracker.prototype.addVote = function(selected) {
+    //   console.dir(selected);
+    // }
+
+
+  //displays the Photos
+    Tracker.prototype.displayPhotos = function() {
+      var rightContent = '<img src="' + this.rightPhoto.path + '" />';
+      var leftContent = '<img src="' + this.leftPhoto.path + '" />';
+      rightPhoto.innerHTML = rightContent;
+      leftPhoto.innerHTML = leftContent;
     }
-   }
-//define the value of both sides of the graph
-   var tracker = new Tracker();
-   catData[0].value = tracker.leftPhoto.vote;
-   catData[1].value = tracker.rightPhoto.vote;
-//Function registered if right pic is clicked
-  Tracker.prototype.rightVote = function(){
-    catChart.segments[0].value++
-    catChart.update();
-    //show reset
-    $('#reset').show();
+    rightPhoto.addEventListener('click', function(e){
+      console.dir(e);
+      tracker.rightVote();
+      tracker.addVote(e.target.attributes[0].value)
+    });
+    leftPhoto.addEventListener('click',tracker.leftVote);
+    reset.addEventListener('click',tracker.reset);
 
+
+
+    tracker.displayPhotos();
   }
-//Function registered if left pic is clicked
-   Tracker.prototype.leftVote = function(){
-    catChart.segments[1].value++
-    catChart.update();
-    //show reset
-    $('#reset').show();
 
-  }
-//Resets the score of the graph and picks a new image
-    Tracker.prototype.reset = function(){
-  catChart.segments[0].value = 1;
-  catChart.segments[1].value = 1;
-  catChart.update();
-  //hide reset
-  cat();
-}
-
-
-//displays the Photos
-  Tracker.prototype.displayPhotos = function() {
-    var rightContent = '<img src="' + this.rightPhoto.path + '" />';
-    var leftContent = '<img src="' + this.leftPhoto.path + '" />';
-    rightPhoto.innerHTML = rightContent;
-    leftPhoto.innerHTML = leftContent;
-  }
-rightPhoto.addEventListener('click',tracker.rightVote);
-leftPhoto.addEventListener('click',tracker.leftVote);
-reset.addEventListener('click',tracker.reset);
-  
-
-tracker.displayPhotos();
-}
 
 
 
